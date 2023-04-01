c7fbba1ce77addef3c4adc405409cac64ff8138d
Dave inoc <michael828inoc@gmail.com>
2023-04-02 03:40:55 +0800

    add backend

---
+++ backend/src/main.ts
@@ -0,0 +1,36 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const rootTypeDefs = gql`
+	type Query {
+		users: [User!]!
+	}
+
+	type User {
+		id: ID!
+		username: String!
+		name: String!
+		email: String!
+	}
+`;
+
+const Query = {
+	users: async () => {
+		return [
+			{
+				id: 1,
+				username: "dave",
+				name: "inoc",
+				email: "daveinoc@gmail.com",
+			},
+		];
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs: rootTypeDefs,
+	resolvers: { Query },
+});
+
+server.listen().then(({ url }) => {
+	console.log(`Server ready on ${url}`);
+});
