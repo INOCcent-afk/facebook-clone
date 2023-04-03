5caab412ec0af04655d6d166da99259336706462
dave <michael828inoc@gmail.com>
2023-04-04 04:33:29 +0800

    remove mobile on dev flow

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Please Work</Text>
+			<Text>Please Work!</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
