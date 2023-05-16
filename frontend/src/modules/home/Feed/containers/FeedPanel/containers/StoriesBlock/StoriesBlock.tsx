a2c08127c86f0de3133616bc9f57ddd0922bfc9a
dave <michael828inoc@gmail.com>
2023-05-16 22:29:11 +0800

    cleanup

--- frontend/src/modules/home/Feed/containers/FeedPanel/containers/StoriesBlock/StoriesBlock.tsx
+++ frontend/src/modules/home/Feed/containers/FeedPanel/containers/StoriesBlock/StoriesBlock.tsx
@@ -1,5 +1,5 @@
 import { Story } from "@/ui";
-import { Box, HStack, Heading, Text } from "@chakra-ui/react";
+import { Box, HStack, Text } from "@chakra-ui/react";
 import React from "react";
 
 export const StoriesBlock = () => {
