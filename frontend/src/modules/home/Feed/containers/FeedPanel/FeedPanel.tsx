4a4afd7f500a2511d7e8099aa17a3b0036480c64
dave <michael828inoc@gmail.com>
2023-07-13 16:18:53 +0800

    input button

--- frontend/src/modules/home/Feed/containers/FeedPanel/FeedPanel.tsx
+++ frontend/src/modules/home/Feed/containers/FeedPanel/FeedPanel.tsx
@@ -2,11 +2,20 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import { StoriesBlock } from "./containers";
 import { FEED_MID_COLUMN_MAX_WIDTH } from "../../utils";
+import { CreateFeed } from "@/ui";
 
 export const FeedPanel = () => {
 	return (
-		<Box flexBasis="60%" maxWidth={FEED_MID_COLUMN_MAX_WIDTH}>
+		<Box
+			display="flex"
+			flexDirection="column"
+			gap={4}
+			flexBasis="60%"
+			maxWidth={FEED_MID_COLUMN_MAX_WIDTH}
+		>
 			<StoriesBlock />
+
+			<CreateFeed />
 		</Box>
 	);
 };
--- frontend/src/ui/index.ts
+++ frontend/src/ui/index.ts
@@ -1,3 +1,4 @@
 export * from "./Header";
 export * from "./SearchFacebook";
 export * from "./Story";
+export * from "./CreateFeed";
