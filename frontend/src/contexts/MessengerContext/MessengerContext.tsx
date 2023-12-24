1c35921885d1f62c43ad797cc3cb9401895dd1aa
dave <michael828inoc@gmail.com>
2023-12-24 19:40:48 +0800

    Merry Christmas

---
+++ frontend/src/contexts/MessengerContext/MessengerContext.tsx
@@ -0,0 +1,33 @@
+import { FC, ReactNode, createContext, useContext, useState } from "react";
+
+interface Context {
+	activeChats: { id: string }[] | null;
+	setActiveChats: (user: { id: string }[]) => void;
+}
+
+export const MessengerContext = createContext<Context>({
+	activeChats: [],
+	setActiveChats: () => {},
+});
+
+interface Props {
+	children?: ReactNode;
+}
+
+export const MessengerProvider: FC<Props> = ({ children }) => {
+	const [activeChats, setActiveChats] = useState<{ id: string }[] | null>(
+		null
+	);
+
+	return (
+		<MessengerContext.Provider value={{ activeChats, setActiveChats }}>
+			{children}
+		</MessengerContext.Provider>
+	);
+};
+
+export const useMessengerState = () => {
+	const messenger = useContext(MessengerContext);
+
+	return { ...messenger };
+};
