a06b1b3ca186a728552e885c364316438b24b8e9
dave <michael828inoc@gmail.com>
2024-01-28 22:50:29 +0800

    gagag

--- backend/src/resolvers/Mutation/Mutation.ts
+++ backend/src/resolvers/Mutation/Mutation.ts
@@ -1,9 +1,11 @@
 import { postResolvers } from "./Post";
 import { userResolvers } from "./User";
 import { notificationResolvers } from "./Notification";
+import { chatResolvers } from "./Chat";
 
 export const Mutation = {
 	...postResolvers,
 	...userResolvers,
 	...notificationResolvers,
+	...chatResolvers,
 };
