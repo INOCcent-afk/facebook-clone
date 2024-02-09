6b2f44a8b1932398027f4df04e2573e8cfb8a6a4
dave <michael828inoc@gmail.com>
2024-02-09 19:59:35 +0800

    I said what i said

--- frontend/src/graphql/mutations/post/sharePost.ts
+++ frontend/src/graphql/mutations/post/sharePost.ts
@@ -3,15 +3,22 @@ import { graphql } from "@/graphql/generated";
 export const sharePost = graphql(/* GraphQL */ `
 	mutation sharePost($post: PostInput!, $sharedPostId: Int!) {
 		sharePost(post: $post, sharedPostId: $sharedPostId) {
-			videos
 			id
-			images
+			images {
+				id
+				userUid
+				image
+			}
 			createdAt
 			postContent
 			sharedPost {
-				videos
 				id
-				images
+				userUid
+				images {
+					id
+					userUid
+					image
+				}
 				createdAt
 			}
 		}
