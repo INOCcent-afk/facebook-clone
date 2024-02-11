3206e6337bec6a28ceab35306eb01d2ca0a82240
dave <michael828inoc@gmail.com>
2024-02-11 19:35:35 +0800

    DAPEK

--- frontend/src/modules/home/Feed/containers/ConversationPanel/ConversationPanel.tsx
+++ frontend/src/modules/home/Feed/containers/ConversationPanel/ConversationPanel.tsx
@@ -1,17 +1,10 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
-import { FriendRequests } from "../FriendRequests";
 import { Messenger } from "../Messenger";
-import { useAuth } from "@/contexts";
 
 export const ConversationPanel = () => {
-	const { user } = useAuth();
-
 	return (
 		<Flex flexDirection="column" gap={4} flexBasis="20%" paddingRight="4">
-			{user && (
-				<FriendRequests friendRequest={user.friendRequestsReceiver} />
-			)}
 			<Messenger />
 		</Flex>
 	);
--- frontend/src/modules/home/Feed/containers/index.ts
+++ frontend/src/modules/home/Feed/containers/index.ts
@@ -1,5 +1,4 @@
 export * from "./ConversationPanel";
 export * from "./FeedPanel";
 export * from "./MenuPanel";
-export * from "./FriendRequests";
 export * from "./Messenger";
